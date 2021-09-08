90914d156cc7a85c0ef3c2d8f2ef3bebb4875bf6
braxtonhall <braxtonjhall@gmail.com>
2021-09-08 15:32:02 -0700

    Minor test cleanup

--- test/SimpleCalculator.spec.ts
+++ test/SimpleCalculator.spec.ts
@@ -1,52 +1,50 @@
 import SimpleCalculator, {TooLarge, TooSimple} from "../src/SimpleCalculator";
 import {testFolder} from "@ubccpsc310/folder-test";
 
-const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised");
+import chai, {expect} from "chai";
+import chaiAsPromised from "chai-as-promised";
 
 chai.use(chaiAsPromised);
-const expect = chai.expect;
 
 type Input = number[];
 type Output = Promise<number>;
 type Error = "TooSimple" | "TooLarge";
 
-describe("SimpleCalculator", () => {
-    describe("Add", () => {
+describe("SimpleCalculator", function () {
+    describe("Add", function () {
 
         let simpleCalculator: SimpleCalculator;
-        beforeEach(() => {
+        beforeEach(function () {
             simpleCalculator = new SimpleCalculator();
         });
 
-        it("should add two numbers", async function() {
+        it("should add two numbers", async function () {
             const result = await simpleCalculator.add([1, 2]);
 
             return expect(result).to.equal(3);
         });
 
-        it("should add negative numbers", async function() {
+        it("should add negative numbers", async function () {
             const result = simpleCalculator.add([-1, -2]);
 
             return expect(result).to.eventually.equal(-3);
         });
 
-        it("should throw TooSimple if array is empty", async function() {
+        it("should throw TooSimple if array is empty", async function () {
             const result = simpleCalculator.add([]);
 
             return expect(result).to.be.rejectedWith(TooSimple);
         });
 
-        it("should throw TooLarge if there is a number above 1000", async function() {
+        it("should throw TooLarge if there is a number above 1000", async function () {
             const result = simpleCalculator.add([1001, 1]);
 
             return expect(result).to.be.rejectedWith(TooLarge);
         });
 
         testFolder<Input, Output, Error>(
-            "Add",
+            "Add Dynamic tests",
             (input: Input): Output => {
-                const simpleCalculator = new SimpleCalculator();
                 return simpleCalculator.add(input);
             },
             "./test/resources",
@@ -59,6 +57,7 @@ describe("SimpleCalculator", () => {
                     } else if (expected === "TooLarge") {
                         expect(actual).to.be.instanceof(TooLarge)
                     } else {
+                        // With the errorValidator, this should be unreachable
                         expect.fail("UNEXPECTED ERROR");
                     }
                 }
