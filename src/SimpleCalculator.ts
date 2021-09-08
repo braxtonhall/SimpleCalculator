8a066a44d40037e466c1d1554bd46dec9c2021b4
Katharine <kekerr@gmail.com>
2021-09-08 13:42:34 -0700

    adding another error and fixing bad code

--- src/SimpleCalculator.ts
+++ src/SimpleCalculator.ts
@@ -5,12 +5,30 @@ export class TooSimple extends Error {
     }
 }
 
+export class TooLarge extends Error {
+    constructor(...args: any[]) {
+        super(...args);
+        Error.captureStackTrace(this, TooLarge);
+    }
+}
+
+const MAX_NUMBER: number = 1000;
+
 export default class SimpleCalculator {
     add(nums: number[]) : Promise<number> {
-        if (nums.length < 1) {
-            return Promise.reject(new TooSimple());
-        }
+        return new Promise<number>((resolve, reject) =>
+        {
+            if (nums.length < 1) {
+                reject(new TooSimple());
+            }
 
-        return Promise.resolve(nums.reduce((prevVal, currentVal) => prevVal + currentVal));
+            nums.forEach(num => {
+                if (num > MAX_NUMBER) {
+                    reject(new TooLarge());
+                }
+            });
+
+            resolve(nums.reduce((prevVal, currentVal) => prevVal + currentVal));
+        });
     }
 }
--- test/SimpleCalculator.spec.ts
+++ test/SimpleCalculator.spec.ts
@@ -1,39 +1,47 @@
-import SimpleCalculator, {TooSimple} from "../src/SimpleCalculator";
+import SimpleCalculator, {TooLarge, TooSimple} from "../src/SimpleCalculator";
 import {testFolder} from "@ubccpsc310/folder-test";
-import exp = require("constants");
 
-var chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 
 chai.use(chaiAsPromised);
-var expect = chai.expect;
+const expect = chai.expect;
 
 type Input = number[];
 type Output = Promise<number>;
-type Error = TooSimple;
+type Error = "TooSimple" | "TooLarge";
 
 describe("SimpleCalculator", () => {
     describe("Add", () => {
-        it("should add two numbers", async() => {
-            const simpleCalculator = new SimpleCalculator();
+
+        let simpleCalculator: SimpleCalculator;
+        beforeEach(() => {
+            simpleCalculator = new SimpleCalculator();
+        });
+
+        it("should add two numbers", async function() {
             const result = await simpleCalculator.add([1, 2]);
 
             return expect(result).to.equal(3);
         });
 
-        it("should add negative numbers", async() => {
-            const simpleCalculator = new SimpleCalculator();
+        it("should add negative numbers", async function() {
             const result = simpleCalculator.add([-1, -2]);
 
             return expect(result).to.eventually.equal(-3);
         });
 
-        it("should throw TooSimple if array is empty", async() => {
-            const simpleCalculator = new SimpleCalculator();
+        it("should throw TooSimple if array is empty", async function() {
             const result = simpleCalculator.add([]);
 
             return expect(result).to.be.rejectedWith(TooSimple);
-        })
+        });
+
+        it("should throw TooLarge if there is a number above 1000", async function() {
+            const result = simpleCalculator.add([1001, 1]);
+
+            return expect(result).to.be.rejectedWith(TooLarge);
+        });
 
         testFolder<Input, Output, Error>(
             "Add",
@@ -43,10 +51,16 @@ describe("SimpleCalculator", () => {
             },
             "./test/resources",
             {
-                errorValidator: (error): error is TooSimple =>
-                    error === "TooSimple",
+                errorValidator: (error): error is Error =>
+                    error === "TooSimple" || error === "TooLarge",
                 assertOnError(expected, actual) {
-                    expect(actual).to.be.instanceof(TooSimple);
+                    if (expected === "TooSimple") {
+                        expect(actual).to.be.instanceof(TooSimple);
+                    } else if (expected === "TooLarge") {
+                        expect(actual).to.be.instanceof(TooLarge)
+                    } else {
+                        expect.fail("UNEXPECTED ERROR");
+                    }
                 }
             }
         )
